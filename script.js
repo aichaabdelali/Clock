fda94a0c26b3537f26405c00a28ec954d7f94420
aichaabdelali <aicha.abdelali6@gmail.com>
2023-01-06 15:01:23 +0100

    fixed night background issue

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ setInterval(function showTime() {
 
   hour.innerHTML = formatTime(hr);
   minutes.innerHTML = formatTime(min);
+
   seconds.innerHTML = formatTime(sec);
 }, 1000);
 
@@ -25,25 +26,25 @@ function switchBg() {
   //morning
   if (current_hour >= 8 && current_hour <= 11) {
     body.style.backgroundImage =
-      'url("https://images.pexels.com/photos/163255/sunrise-sun-morgenrot-skies-163255.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")';
+      'url("https://images.unsplash.com/photo-1445985543470-41fba5c3144a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80")';
   }
 
   //noon
   if (current_hour >= 12 && current_hour <= 15) {
     body.style.backgroundImage =
-      'url("https://images.pexels.com/photos/2872210/pexels-photo-2872210.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")';
+      'url("https://images.unsplash.com/photo-1460036521480-ff49c08c2781?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=873&q=80")';
   }
 
   //afternoon
   if (current_hour >= 16 && current_hour <= 19) {
     body.style.backgroundImage =
-      'url("https://images.pexels.com/photos/1669939/pexels-photo-1669939.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")';
+      'url("https://images.unsplash.com/photo-1511671782779-c97d3d27a1d4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80")';
   }
 
   //night
-  if (current_hour >= 20 && current_hour <= 7) {
+  if (current_hour >= 20 || current_hour <= 7) {
     body.style.backgroundImage =
-      'url("https://images.pexels.com/photos/355465/pexels-photo-355465.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")';
+      'url("https://images.unsplash.com/photo-1470019693664-1d202d2c0907?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8cmVoZXJzYWwlMjBtdXNpY3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=700&q=60")';
   }
 }
 
